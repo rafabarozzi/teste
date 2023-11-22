868b9a8da3082545725bf8dbed45fdc8a8af9794
rafabarozzi <rafael.barozzi@gmail.com>
2023-11-21 22:52:11 -0300

    dev

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello From Go 3!!")
+		fmt.Fprintf(w, "Hello From Go - DEV!!")
 	})
 
 	fmt.Printf("Server is running on port 3000...")
